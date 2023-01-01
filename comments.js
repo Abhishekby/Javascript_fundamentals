739faf76cfec3cc17f186871586a43c13897b78e
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2023-01-01 13:10:14 +0530

    Create comments.js

---
+++ comments.js
@@ -0,0 +1,22 @@
+/* The following program contains source code for a game called Tic-tac-toe.
+It is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. 
+The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner
+*/
+
+/**
+* The following program contains source code for a game called Tic-tac-toe.
+* It is a paper-and-pencil game for two players, X and O, who take turns marking the
+* spaces in a 3×3 grid. 
+* The player who succeeds in placing three of their marks in a horizontal, vertical, or 
+* diagonal row is the winner
+*/
+
+/**
+* Function that greets a user
+* @author   John
+* @param    {String} name    Name of the user
+* @return   {String}         Greeting message
+*/
+function greetUser(name) {
+    return `Greetings, ${name}!`;
+}
