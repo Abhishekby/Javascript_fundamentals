88ab8609feb259fdc96e4e05a4d21aa58d9285b3
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2023-01-05 02:00:18 +0530

    Create classes.js

---
+++ classes.js
@@ -0,0 +1,12 @@
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+//The example above creates a class named "Car".
+
+//The class has two initial properties: "name" and "y
+let myCar1 = new Car("Ford", 2014);
+let myCar2 = new Car("Audi", 2019);
+//The example above uses the Car class to create two Car objects.
