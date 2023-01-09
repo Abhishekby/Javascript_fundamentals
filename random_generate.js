6319a7be4af4c7b7c3620900d1f95ec22535dba0
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2023-01-09 17:23:16 +0530

    Create random_generate.js

---
+++ random_generate.js
@@ -0,0 +1,9 @@
+// generating  a random number
+const a = Math.random();
+console.log(a);
+//Here, we have declared a variable a and assigned it a random number greater than or equal to 0 and less than 1.
+
+//Example 2: Get a Random Number between 1 and 10
+// generating a random number
+const a = Math.random() * (10-1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
