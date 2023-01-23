51e34175159ff2dd2053669b9334c6d440cec1b9
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2023-01-24 02:14:52 +0530

    Update classes.js

--- classes.js
+++ classes.js
@@ -9,4 +9,6 @@ class Car {
 //The class has two initial properties: "name" and "y
 let myCar1 = new Car("Ford", 2014);
 let myCar2 = new Car("Audi", 2019);
+let myCar2 = new Car("Audi", 2019);
+
 //The example above uses the Car class to create two Car objects.
